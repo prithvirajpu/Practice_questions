405ce9d873b41a456a3afdb074b6e2ca03d48fac
prithviraj <prithvirajpu999@gmail.com>
2025-09-03 21:30:56 +0530

    some more updates

--- Binary_search_tree.py
+++ Binary_search_tree.py
@@ -6,23 +6,106 @@ class Node:
 class BST:
     def __init__(self):
         self.root=None
+    
+    # insertion
     def insert(self,data):
         if self.root is None:
             self.root=Node(data)
+            return
         else:
             self._insert(self.root,data)
-    def _insert(self,temp,data):
-        if data<temp.data:
-            if temp.left is None:
-                temp.left=Node(data)
+    def _insert(self,root,data):
+        if data<root.data:
+            if root.left is None:
+                root.left=Node(data)
             else:
-                self._insert(temp.left,data)
-        elif data>temp.data:
-            if temp.right is None:
-                temp.right=Node(data)
+                self._insert(root.left,data)
+        elif data>root.data:
+            if root.right is None:
+                root.right=Node(data)
             else:
-                self._insert(self.right,data)
+                self._insert(root.right,data)
 
+    # Searching
+    def search(self,root,data):
+        if root is None:
+            return False
+        if root.data==data:
+            return True
+        elif data<root.data:
+            return self.search(root.left,data)
+        elif data>root.data:
+            return self.search(root.right,data)
+        
+    #  preorder
+    #  postorder
+    #  inorder   
+    def preorder(self,root):
+        if root is None:
+            return
+        print(root.data, end='-')
+        self.preorder(root.left)
+        self.preorder(root.right)
+    def postorder(self,root):
+        if root is None:
+            return
+        self.postorder(root.left)
+        self.postorder(root.right)
+        print(root.data,end='-')
+    def inorder(self,root):
+        if root is None:
+            return
+        self.inorder(root.left)
+        print(root.data,end='-')
+        self.inorder(root.right)
+
+    def deleteNode(self, root, data):
+        if root is None:
+            return None
+
+        if data < root.data:
+            root.left = self.deleteNode(root.left, data)
+        elif data > root.data:
+            root.right = self.deleteNode(root.right, data)
+        else:
+            # Case 1: No children (leaf node)
+            if root.left is None and root.right is None:
+                return None
+
+            # Case 2: One child (right)
+            elif root.left is None:
+                return root.right
+
+            # Case 2: One child (left)
+            elif root.right is None:
+                return root.left
+
+            # Case 3: Two children
+            else:
+                mindata = self.minValue(root.right)  # find inorder successor
+                root.data = mindata                  # replace value
+                root.right = self.deleteNode(root.right, mindata)  # delete successor
+        return root
+
+    def minValue(self, node):
+        current = node
+        while current.left:   # keep going left to find min
+            current = current.left
+        return current.data
+
+            
+        
 x=BST()
 x.insert(10)
-x.insert(120)
+x.insert(20)
+x.insert(30)
+x.insert(40)
+x.insert(8)
+print(x.search(x.root,40))
+print(x.search(x.root,60))
+x.preorder(x.root)
+print()
+x.postorder(x.root)
+print()
+x.deleteNode(x.root,10)
+x.inorder(x.root)
