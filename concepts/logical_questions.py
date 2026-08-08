2b05b4cb6c1e356d3da217c22291dea3101a0e5c
prithviraj <prithvirajpu999@gmail.com>
2026-08-08 21:52:53 +0530

    add more problems

---
+++ concepts/logical_questions.py
@@ -0,0 +1,22 @@
+# move the zeros to the end
+arr = [3, 4, 0, 0, 6, 0, 7, 0, 0, 8]
+
+j=0
+for i in range(len(arr)):
+    if arr[i]!=0:
+        arr[i],arr[j]=arr[j],arr[i]
+        j+=1
+print(arr)
+
+# find the extra value
+arr1=[0,1,2,3]
+arr2=[0,1,2,3,4]
+
+res=0
+for i in arr1:
+    res^=i
+for i in arr2:
+    res^=i
+    
+print(res)
+
